b8f7f58246f9dc83bdc6479b42a555f484e038cf
ProGRAMmisT <apocalypsisdimon@gmail.com>
2021-07-22 21:28:40 +0500

    Добавлен блок банера на главную страницу.

--- index.php
+++ index.php
@@ -1,2 +1,51 @@
 <?php include(__DIR__.'/parts/header.php') ?>
+<div class="page" data-page="main">
+    <div class="block" data-block="banner">
+        <div class="block-slide" style="background-image: url('<?= SITE_TEMPLATE_PATH ?>/assets/resources/pictures/main.banner.jpg')">
+            <div class="block-slide-overlay"></div>
+            <div class="block-slide-content template-delimiter">
+                <div class="block-slide-content-wrapper template-delimiter-wrapper">
+                    <div class="block-slide-information">
+                        <div class="block-slide-title">Группа компаний</div>
+                        <div class="block-slide-line"></div>
+                        <div class="block-slide-description">С экспертизой в области красоты и здоровья человека</div>
+                    </div>
+                    <div class="block-slide-advantages">
+                        <div class="block-slide-advantage">
+                            <div class="block-slide-advantage-wrapper">
+                                <div class="block-slide-advantage-title">100+</div>
+                                <div class="block-slide-advantage-description">На рынке бьюти</div>
+                                <div class="block-slide-advantage-line"></div>
+                            </div>
+                        </div>
+                        <div class="block-slide-advantage">
+                            <div class="block-slide-advantage-wrapper">
+                                <div class="block-slide-advantage-title">18</div>
+                                <div class="block-slide-advantage-description">Обученных специалистов</div>
+                                <div class="block-slide-advantage-line"></div>
+                            </div>
+                        </div>
+                        <div class="block-slide-advantage">
+                            <div class="block-slide-advantage-wrapper">
+                                <div class="block-slide-advantage-title">900+</div>
+                                <div class="block-slide-advantage-description">Партнеров - салонов и клиник красоты*</div>
+                                <div class="block-slide-advantage-line"></div>
+                            </div>
+                        </div>
+                        <div class="block-slide-advantage">
+                            <div class="block-slide-advantage-wrapper">
+                                <div class="block-slide-advantage-title">80</div>
+                                <div class="block-slide-advantage-description">Франчайзи</div>
+                                <div class="block-slide-advantage-line"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="block-slide-arrow">
+                        <img src="<?= SITE_TEMPLATE_PATH ?>/assets/resources/icons/arrow.rounded.down.svg" alt="Смотреть" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <?php include(__DIR__.'/parts/footer.php') ?>
