069c3d034bb83df1d6d95b4ffc69bb3a883e11d9
ProGRAMmisT <apocalypsisdimon@gmail.com>
2021-07-23 01:02:40 +0500

    Добавлен блок франчайзор на главную страницу. Доработан блок дистрибьютор.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include(__DIR__.'/parts/header.php') ?>
 <div class="page" data-page="main">
     <div class="block" data-block="banner">
-        <div class="block-slide" style="background-image: url('<?= SITE_TEMPLATE_PATH ?>/assets/resources/pictures/main.banner.jpg')">
+        <div class="block-slide" style="background-image: url('<?= SITE_TEMPLATE_PATH ?>/assets/resources/pictures/pages/main/banner.jpg')">
             <div class="block-slide-overlay"></div>
             <div class="block-slide-content template-delimiter">
                 <div class="block-slide-content-wrapper template-delimiter-wrapper">
@@ -52,11 +52,149 @@
             <div class="block-header-title">Дистрибьютор</div>
             <div class="block-header-line"></div>
         </div>
-        <div class="block-content">Представляем в России мировые бренды топ-уровня, каждый из которых ценен и уникален: профессиональной косметики и микронутриентов премиум класса, бьюти-гаджеты и ароматы для дома.</div>
+        <div class="block-text">Представляем в России мировые бренды топ-уровня, каждый из которых ценен и уникален: профессиональной косметики и микронутриентов премиум класса, бьюти-гаджеты и ароматы для дома.</div>
         <div class="block-buttons">
             <a href="#" class="block-button control" data-control="button">
                 <span class="control-text">Стать партнёром</span>
             </a>
+        </div>
+        <div class="block-partners">
+            <div class="block-partners-background">
+                <div class="block-partners-background--left"></div>
+                <div class="block-partners-background--right"></div>
+            </div>
+            <div class="block-partners-content template-delimiter">
+                <div class="block-partners-content-wrapper template-delimiter-wrapper">
+                    <div class="block-partners--left">
+                        <div class="block-partners-header">
+                            <div class="block-partners-title">Beauty</div>
+                            <div class="block-partners-line"></div>
+                        </div>
+                        <div class="block-partners-items">
+                            <div class="block-partners-item">
+                                <div class="block-partners-item-wrapper">
+                                    <div class="block-partners-item-wrapper-2">
+                                        <div class="block-partners-item-wrapper-3">
+                                            <img src="<?= SITE_TEMPLATE_PATH ?>/assets/resources/pictures/pages/main/partners/babor.png" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="block-partners-item">
+                                <div class="block-partners-item-wrapper">
+                                    <div class="block-partners-item-wrapper-2">
+                                        <div class="block-partners-item-wrapper-3">
+                                            <img src="<?= SITE_TEMPLATE_PATH ?>/assets/resources/pictures/pages/main/partners/cosmedix.png" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="block-partners-item">
+                                <div class="block-partners-item-wrapper">
+                                    <div class="block-partners-item-wrapper-2">
+                                        <div class="block-partners-item-wrapper-3">
+                                            <img src="<?= SITE_TEMPLATE_PATH ?>/assets/resources/pictures/pages/main/partners/stbarth.png" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="block-partners-item">
+                                <div class="block-partners-item-wrapper">
+                                    <div class="block-partners-item-wrapper-2">
+                                        <div class="block-partners-item-wrapper-3">
+                                            <img src="<?= SITE_TEMPLATE_PATH ?>/assets/resources/pictures/pages/main/partners/pmd.png" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="block-partners--right">
+                        <div class="block-partners-header">
+                            <div class="block-partners-title">Health</div>
+                            <div class="block-partners-line"></div>
+                        </div>
+                        <div class="block-partners-items">
+                            <div class="block-partners-item">
+                                <div class="block-partners-item-wrapper">
+                                    <div class="block-partners-item-wrapper-2">
+                                        <div class="block-partners-item-wrapper-3">
+                                            <img src="<?= SITE_TEMPLATE_PATH ?>/assets/resources/pictures/pages/main/partners/anp.png" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="block-partners-item">
+                                <div class="block-partners-item-wrapper">
+                                    <div class="block-partners-item-wrapper-2">
+                                        <div class="block-partners-item-wrapper-3">
+                                            <img src="<?= SITE_TEMPLATE_PATH ?>/assets/resources/pictures/pages/main/partners/biogena.png" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="block-partners-item">
+                                <div class="block-partners-item-wrapper">
+                                    <div class="block-partners-item-wrapper-2">
+                                        <div class="block-partners-item-wrapper-3">
+                                            <img src="<?= SITE_TEMPLATE_PATH ?>/assets/resources/pictures/pages/main/partners/poo.png" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="block-partners-item">
+                                <div class="block-partners-item-wrapper">
+                                    <div class="block-partners-item-wrapper-2">
+                                        <div class="block-partners-item-wrapper-3">
+                                            <img src="<?= SITE_TEMPLATE_PATH ?>/assets/resources/pictures/pages/main/partners/candle.png" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="block" data-block="franchise">
+        <div class="block-header">
+            <div class="block-header-title">Франчайзор</div>
+            <div class="block-header-line"></div>
+        </div>
+        <div class="block-text">Вдохновляем и предлагаем проверенные бизнес-решения для салонов и клиник красоты.</div>
+        <div class="block-buttons">
+            <a href="#" class="block-button control" data-control="button" data-control-scheme="transparent.1">
+                <span class="control-text">Получить предложение по франшизе</span>
+            </a>
+        </div>
+        <div class="block-franchises">
+            <div class="block-franchises-items">
+                <div class="block-franchises-item">
+                    <a href="#" class="block-franchises-item-picture">
+                        <span class="block-franchises-item-picture-wrapper" style="background-image: url('<?= SITE_TEMPLATE_PATH ?>/assets/resources/pictures/pages/main/franchises/beauty.png')"></span>
+                    </a>
+                    <div class="block-franchises-item-information">
+                        <a href="#" class="block-franchises-item-title">
+                            <span>Beauty</span>
+                            <span>- франшиза института красоты Babor</span>
+                        </a>
+                        <div class="block-franchises-item-description">Разработка и производство ведутся в нашей штаб-квартире в Ахене, поэтому каждый продукт – от идеи до готовой баночки – живое воплощение принципа made in Germany.</div>
+                    </div>
+                </div>
+                <div class="block-franchises-item">
+                    <div class="block-franchises-item-information">
+                        <a href="#" class="block-franchises-item-title">
+                            <span>Health</span>
+                            <span>- франшиза Австрийской клиники превентивной медицины BIOGENA</span>
+                        </a>
+                        <div class="block-franchises-item-description">Разработка и производство ведутся в нашей штаб-квартире в Ахене, поэтому каждый продукт – от идеи до готовой баночки.</div>
+                    </div>
+                    <a href="#" class="block-franchises-item-picture">
+                        <span class="block-franchises-item-picture-wrapper" style="background-image: url('<?= SITE_TEMPLATE_PATH ?>/assets/resources/pictures/pages/main/franchises/health.png')"></span>
+                    </a>
+                </div>
+            </div>
         </div>
     </div>
 </div>
