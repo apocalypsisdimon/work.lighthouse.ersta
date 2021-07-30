240776d04fcaef71e77b55162d9980fc6d44dd48
ProGRAMmisT <apocalypsisdimon@gmail.com>
2021-07-30 23:08:16 +0500

    Добавлена 404 страница.

---
+++ pages/404.php
@@ -0,0 +1,18 @@
+<?php include(__DIR__.'/../parts/header.php') ?>
+<div class="page template-delimiter" data-page="404">
+    <div class="template-delimiter-wrapper">
+        <div class="template-layout" data-template-layout="simple">
+            <div class="page-picture">
+                <img src="<?= SITE_TEMPLATE_PATH ?>/assets/resources/pictures/pages/404/banner.png" />
+            </div>
+            <h3 class="page-title">Cтраница не найдена</h3>
+            <div class="page-description">Что-то пошло не так — страница, которую вы хотели открыть, не существует.</div>
+            <div class="page-controls">
+                <div class="page-control">
+                    <a href="#" class="control" data-control="button" data-control-block="true">Вернуться на главную</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include(__DIR__.'/../parts/footer.php') ?>
